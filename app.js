eec739069902234457331fd879c023df3188add7
shubhamxpatel <48165598+shubhamxpatel@users.noreply.github.com>
2021-01-13 15:12:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ var app = express();
 app.use(express.static('/public'))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
