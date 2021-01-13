1e786542996c1b90667e684947ad90a7041fc1f8
shubhamxpatel <48165598+shubhamxpatel@users.noreply.github.com>
2021-01-13 20:22:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.static('/public'))
 app.set('views', path.join(__dirname, 'views'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods" , "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   next();
 });
 
