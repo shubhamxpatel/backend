c928ba6bface9e21cd66a59d7401b851e8f04bd1
shubhamxpatel <sp1651999@gmai.com>
2021-05-04 20:53:41 +0530

    shdbdsdsjds

--- routes/login.js
+++ routes/login.js
@@ -21,7 +21,8 @@ async function fun(req, response) {
 
 
         })
-        response.send({ "res": "login successful", "id": ans[0]._id, "cook": "response.cookies" })
+        console.log(response.cookies)
+        response.send({ "res": "login successful", "id": ans[0]._id, "cook": "response.cookies", "rumor": response.cookies })
             //response.send({ "res": "login successful", "id": ans[0]._id })
         await conn.collection("login").updateMany({ "_id": mongodb.ObjectID(ans[0]._id) }, { $set: { "logStatus": 1 } })
             .then(res2 => {
