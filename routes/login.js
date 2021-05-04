4ba91ce678a7308805f878e087afdd467f27655f
shubhamxpatel <sp1651999@gmai.com>
2021-05-04 17:02:51 +0530

    shdbj

--- routes/login.js
+++ routes/login.js
@@ -1,58 +1,64 @@
 var express = require('express');
 var router = express.Router();
-var path=require('path')
-var mongodb=require('mongodb')
+var path = require('path')
+var mongodb = require('mongodb')
 
 //console.log(path.join(__dirname,'../public'))
 var conn = require("../public/javascripts/connect.js")
-var ans=""
+var ans = ""
 
 /* GET users listing. */
-async function fun(req,response){
-    let cursor=await conn.collection("login").find({"user":req.email,"pass":req.pass})
-    ans=await cursor.toArray()
-    if(ans.length>0){
+async function fun(req, response) {
+    let cursor = await conn.collection("login").find({ "user": req.email, "pass": req.pass })
+    ans = await cursor.toArray()
+    if (ans.length > 0) {
         console.log(ans)
-        response.send({"res":"login successful","id":ans[0]._id})
-        await conn.collection("login").updateMany({"_id":mongodb.ObjectID(ans[0]._id)},{$set:{"logStatus":1}})
-        .then(res2=>{
-            response.send({"res":"login successful","id":ans[0]._id})
-        })
-        .catch(err=>{
-            response.send({"res":"login unsuccessful"})
-        })
-        
-    }
-    else{
-        response.send({"res":"login unsuccessful"})
+        response.send({ "res": "login successful", "id": ans[0]._id })
+        await conn.collection("login").updateMany({ "_id": mongodb.ObjectID(ans[0]._id) }, { $set: { "logStatus": 1 } })
+            .then(res2 => {
+                response.cookie('name', resp.admin, {
+                    expires: new Date(Date.now() + 300000),
+                    httpOnly: true,
+                    //secure: true,
+
+                    path: '/'
+                })
+                response.send({ "res": "login successful", "id": ans[0]._id })
+            })
+            .catch(err => {
+                response.send({ "res": "login unsuccessful" })
+            })
+
+    } else {
+        response.send({ "res": "login unsuccessful" })
     }
     //response.send(ans)
-	//console.log(p["ops"])
-	//ans = p["ops"]
+    //console.log(p["ops"])
+    //ans = p["ops"]
 }
-async function fun1(id,res){
-    await conn.collection("login").updateMany({"_id":mongodb.ObjectID(id)},{$set:{"logStatus":0}}).then(res1=>{
-        res.send({"res":1})
-    })
-    .catch(err=>{
-        res.send({"res":0})
-    })
+async function fun1(id, res) {
+    await conn.collection("login").updateMany({ "_id": mongodb.ObjectID(id) }, { $set: { "logStatus": 0 } }).then(res1 => {
+            res.send({ "res": 1 })
+        })
+        .catch(err => {
+            res.send({ "res": 0 })
+        })
 }
 router.post('/in', function(req, res, next) {
-  let id=""
-  console.log(req.body)
-  fun(req.body,res)
-  //console.log(ans)
-  //res.send(ans);
-  //console.log(res)
+    let id = ""
+    console.log(req.body)
+    fun(req.body, res)
+        //console.log(ans)
+        //res.send(ans);
+        //console.log(res)
 });
 router.get('/out/:id', function(req, res, next) {
-    let id=""
+    let id = ""
     console.log(req.params.id)
-    fun1(req.params.id,res)
-    //console.log(ans)
-    //res.send(ans);
-    //console.log(res)
-  });
+    fun1(req.params.id, res)
+        //console.log(ans)
+        //res.send(ans);
+        //console.log(res)
+});
 
 module.exports = router;
