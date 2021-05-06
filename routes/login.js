7254606dd716e7e45b5727b1195f4534cbc34e5d
shubhamxpatel <sp1651999@gmai.com>
2021-05-07 02:26:13 +0530

    dsk

--- routes/login.js
+++ routes/login.js
@@ -22,14 +22,10 @@ async function fun(req, response) {
         //         // domain: 'localhost:5000',
         //         path: '/'
         //     })
-        await response.setHeader("set-cookie", `auth_token=token; samesite=none; expires=${new Date(Date.now() + 300000)}; secure; httpOnly`)
-            .then(() => {
-                console.log(response.req.rawHeaders)
-                response.send({ "res": "login successful", "id": ans[0]._id, "cook": response.req.rawHeaders })
-            })
-
-
-        //response.send({ "res": "login successful", "id": ans[0]._id })
+        // await response.setHeader("set-cookie", `auth_token=token; samesite=none; expires=${new Date(Date.now() + 300000)}; secure; httpOnly`)
+        //console.log(response.req.rawHeaders)
+        response.send({ "res": "login successful", "id": ans[0]._id, "cook": response.req.rawHeaders })
+            //response.send({ "res": "login successful", "id": ans[0]._id })
         await conn.collection("login").updateMany({ "_id": mongodb.ObjectID(ans[0]._id) }, { $set: { "logStatus": 1 } })
             .then(res2 => {
                 // console.log(res2)
