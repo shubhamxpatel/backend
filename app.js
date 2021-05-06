3bdd4e47e1bbc6e4d08c8eb0e3fb70b1d2f0ce71
shubhamxpatel <sp1651999@gmai.com>
2021-05-07 02:13:53 +0530

    dsk

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60, // two weeks
-        sameSite = 'none',
+        sameSite: 'none',
         secure: true
     }
 }));
